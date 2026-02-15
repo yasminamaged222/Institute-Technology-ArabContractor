192c37a35aa7deafe192cce6a426e922b54a0d4f
Amr Shamy <104179365+Shaaamy@users.noreply.github.com>
2026-02-15 12:27:01 +0200

    Add Service and Controller For Checking Out For Authenticated Users

---
+++ src/Backend/Institute.Web/Institute.Application/Interfaces/IService/ICheckoutService.cs
@@ -0,0 +1,12 @@
+﻿using Institute.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Institute.Application.Interfaces.IService
+{
+    public interface ICheckoutService
+    {
+        Task<Order> CreateOrderAsync(int userId);
+        Task<Payment> ProcessPaymentAsync(int orderId, string transactionRef, string gatewayResponse, bool success);
+        Task<AppUser> GetUserByClerkIdAsync(string clerkUserId);
+    }
+}
