164e076b19375920624c409bfef3ea99a36a39d0
yasminamaged222 <yasminamaged22@gmail.com>
2026-03-04 11:50:31 +0200

    main.jsx

--- src/Frontend/src/main.jsx
+++ src/Frontend/src/main.jsx
@@ -1,46 +1,3 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom/client';
-//import { BrowserRouter } from 'react-router-dom';
-//import { ThemeProvider } from '@mui/material/styles';
-//import { CacheProvider } from '@emotion/react';
-//import CssBaseline from '@mui/material/CssBaseline';
-//import theme, { cacheRtl } from './theme/theme.js';
-//import './index.css';
-//import Navbar from './components/Navbar.jsx';
-//import Footer from './components/Footer.jsx';
-//import App from './App.jsx';
-//import { ClerkProvider } from '@clerk/clerk-react'
-//import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
-//import ErrorBoundary from './pages/ErrorBoundary.jsx';
-
-//const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuYWN3ZWJzaXRlLWljbWV0LXRlc3QuYXp1cmV3ZWJzaXRlcy5uZXQk"
-
-
-//if (!PUBLISHABLE_KEY) {
-//    throw new Error('Add your Clerk Publishable Key to the .env file')
-//}
-
-
-//ReactDOM.createRoot(document.getElementById('root')).render(
-//  //<React.StrictMode>
-//        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-//            <CacheProvider value={cacheRtl}>
-//                <ThemeProvider theme={theme}>
-//                    <CssBaseline />
-//                    <BrowserRouter>
-//                    <div dir="rtl">
-//                        <ErrorBoundary>
-
-//                            <App />
-//                        </ErrorBoundary>
-//                        </div>
-//                    </BrowserRouter>
-//                </ThemeProvider>
-//            </CacheProvider>
-//        </ClerkProvider>
-//   //</React.StrictMode>
-//);
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -56,30 +13,30 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 import ErrorBoundary from './pages/ErrorBoundary.jsx';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuYWN3ZWJzaXRlLWljbWV0LXRlc3QuYXp1cmV3ZWJzaXRlcy5uZXQk"
+
 
 if (!PUBLISHABLE_KEY) {
     throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    //<React.StrictMode>
-    <ClerkProvider
-        publishableKey={PUBLISHABLE_KEY}
-        proxyUrl="https://acwebsite-icmet-test.azurewebsites.net/clerk-proxy"
-    >
-        <CacheProvider value={cacheRtl}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <BrowserRouter>
+  //<React.StrictMode>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+            <CacheProvider value={cacheRtl}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <BrowserRouter>
                     <div dir="rtl">
                         <ErrorBoundary>
+
                             <App />
                         </ErrorBoundary>
-                    </div>
-                </BrowserRouter>
-            </ThemeProvider>
-        </CacheProvider>
-    </ClerkProvider>
-    //</React.StrictMode>
+                        </div>
+                    </BrowserRouter>
+                </ThemeProvider>
+            </CacheProvider>
+        </ClerkProvider>
+   //</React.StrictMode>
 );
