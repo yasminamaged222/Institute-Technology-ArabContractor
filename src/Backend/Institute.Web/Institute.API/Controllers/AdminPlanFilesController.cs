7774c868b1d2a03523434611076d1ee55816663a
Shaaamy <amrshamy91@gmail.com>
2026-05-10 10:20:59 +0300

    Add Admin PlanFiles service and controller

---
+++ src/Backend/Institute.Web/Institute.API/Controllers/AdminPlanFilesController.cs
@@ -0,0 +1,49 @@
+﻿using Institute.Application.DTOs.AdminDtos;
+using Institute.Application.Interfaces.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Institute.API.Controllers
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class AdminPlanFilesController : ControllerBase
+    {
+        private readonly IPlanFileService _service;
+
+        public AdminPlanFilesController(IPlanFileService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{planId}")]
+        public async Task<IActionResult> GetFiles(int planId)
+        {
+            var result = await _service.GetFilesAsync(planId);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(
+            [FromForm] CreatePlanFileDto dto)
+        {
+            await _service.AddAsync(dto);
+
+            return Ok(new
+            {
+                message = "File uploaded successfully"
+            });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(
+            int planId,
+            int fileId)
+        {
+            await _service.DeleteAsync(planId, fileId);
+
+            return Ok();
+        }
+    }
+}
---
+++ src/Backend/Institute.Web/Institute.Application/DTOs/AdminDtos/PlanFileDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Institute.Application.DTOs.AdminDtos
+{
+    public class PlanFileDto
+    {
+        public int PlanId { get; set; }
+
+        public int FileId { get; set; }
+
+        public string? FileTitle { get; set; }
+
+        public string? FileName { get; set; }
+
+        public int? FilePeriorty { get; set; }
+
+        // optional
+        public string? PlanworkName { get; set; }
+    }
+}
