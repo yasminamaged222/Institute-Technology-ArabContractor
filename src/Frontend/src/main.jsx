f375aa22875c2f9fee57f355baac3465effe63d6
yasmina <yasmina@02-YASMINA>
2026-05-13 13:18:08 +0300

    pklive updated

--- src/Frontend/src/main.jsx
+++ src/Frontend/src/main.jsx
@@ -6,37 +6,29 @@ import { CacheProvider } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme, { cacheRtl } from './theme/theme.js';
 import './index.css';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
 import App from './App.jsx';
-import { ClerkProvider } from '@clerk/clerk-react'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import { ClerkProvider } from '@clerk/clerk-react';
 import ErrorBoundary from './pages/ErrorBoundary.jsx';
 
-const PUBLISHABLE_KEY = "pk_test_bWlnaHR5LWJhc2lsaXNrLTExLmNsZXJrLmFjY291bnRzLmRldiQ"
-
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-    throw new Error('Add your Clerk Publishable Key to the .env file')
+    throw new Error('Add your Clerk Publishable Key to the .env file');
 }
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-            <CacheProvider value={cacheRtl}>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <CacheProvider value={cacheRtl}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <BrowserRouter>
                     <div dir="rtl">
                         <ErrorBoundary>
-
                             <App />
                         </ErrorBoundary>
-                        </div>
-                    </BrowserRouter>
-                </ThemeProvider>
-            </CacheProvider>
-        </ClerkProvider>
-   //</React.StrictMode>
+                    </div>
+                </BrowserRouter>
+            </ThemeProvider>
+        </CacheProvider>
+    </ClerkProvider>
 );
